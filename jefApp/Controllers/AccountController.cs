5b47af257162ab22e9853627ae6be531e3dd4141
jefenso <jaespiritu@student.apc.edu.ph>
2023-10-08 17:32:16 +0800

    Add project files.

---
+++ jefApp/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using jefApp.Models.EntityManager;
+using jefApp.Models.ViewModel;
+
+namespace jefApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            UserManager um = new UserManager();
+            UsersModel user = um.GetAllUsers();
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            ModelState.Remove("AccountImage");
+
+            if (ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                if (!um.IsLoginNameExist(user.LoginName))
+                {
+                    um.AddUserAccount(user);
+                    // FormsAuthentication.SetAuthCookie(user.FirstName, false);
+                    return RedirectToAction("", "Index");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update([FromBody] UserModel userData)
+        {
+            UserManager um = new UserManager();
+            if (um.IsLoginNameExist(userData.LoginName))
+            {
+                um.UpdateUserAccount(userData);
+                return RedirectToAction("Index"); // Redirect to a relevant action after successful update.
+            }
+            // Handle the case when the login name doesn't exist, e.g., return a relevant error view.
+            return RedirectToAction("LoginNameNotFound");
+        }
+
+    }
+}
